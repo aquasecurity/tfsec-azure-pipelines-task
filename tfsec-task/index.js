6dd4d0b324827ccc88d51c57a78326aa7ed0b738
Liam Galvin <liam.galvin@aquasec.com>
2022-06-21 11:29:04 +0100

    feat: Customise directory to scan

--- tfsec-task/index.js
+++ tfsec-task/index.js
@@ -1,7 +1,11 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -66,7 +70,13 @@ function run() {
             }
             runner.arg(["-f", "junit,json"]);
             runner.arg(["-O", outputPath]);
-            runner.arg(task.cwd());
+            let dir = task.getInput("dir", false);
+            if (dir !== undefined) {
+                runner.line(dir);
+            }
+            else {
+                runner.arg(task.cwd());
+            }
             console.log("Running tfsec...");
             let result = runner.execSync();
             if (result.code === 0) {
